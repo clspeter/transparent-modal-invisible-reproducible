e08b32645a9c8d90021423ee7ccaaee18fd8f352
mustpe <mustpe@gmail.com>
2025-02-04 17:18:49 +0800

    clean up

--- app/index.tsx
+++ app/index.tsx
@@ -5,17 +5,8 @@ import { router } from "expo-router";
 export default function HomeScreen() {
   return (
     <View style={{ gap: 16 }}>
-      <Text>Start Here</Text>
+      <Text>go to Stack0</Text>
       <Button onPress={() => router.navigate("/stack")} title="Stack0" />
-      <Text>
-        This bug needs to init in a nested stack, will not happened from here
-      </Text>
-      <Button onPress={() => router.navigate("/stack1")} title="Stack1" />
-      <Button onPress={() => router.navigate("/stack2")} title="Stack2" />
-      <Button
-        onPress={() => router.navigate("/stack/modal-page")}
-        title="Transparent Modal In Stack0"
-      />
     </View>
   );
 }
